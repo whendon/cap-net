1b64d17966d9dc4ac63f207de2c818ca8c933ddf
vltadrian <adrian.vultur@gmail.com>
2014-07-04 17:32:00 +0300

    Addresses required when scope is private Validator
    Tests Added

---
+++ src/CAPNet.Tests/ValidatorTests/Required/AddressesRequiredWhenScopeValidatorTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xunit;
+using CAPNet.Tests;
+using CAPNet.Models;
+
+namespace CAPNet
+{
+    public class AddressesRequiredWhenScopeValidatorTests
+    {
+        [Fact]
+        public void AlertWithAddressAndNoScopeIsValid()
+        {
+            var alert = new Alert();
+            alert.Addresses = "list1 list2 list3";
+
+            var addressesRequiredWhenScopeValidator = new AddressesRequiredWhenScopeValidator(alert);
+            Assert.True(addressesRequiredWhenScopeValidator.IsValid);
+        }
+
+        [Fact]
+        public void AlertWithAddressesAndScopePrivateIsValid()
+        {
+            var alert = new Alert();
+            alert.Addresses = "list1 list2 list3";
+            alert.Scope = Scope.Private;
+
+            var addressesRequiredWhenScopeValidator = new AddressesRequiredWhenScopeValidator(alert);
+            Assert.True(addressesRequiredWhenScopeValidator.IsValid);
+        }
+
+        [Fact]
+        public void AlertWithAddressesAndScopeNoPrivateIsValid()
+        {
+            var alert = new Alert();
+            alert.Addresses = "list1 list2 list3";
+            alert.Scope = Scope.Public;
+
+            var addressesRequiredWhenScopeValidator = new AddressesRequiredWhenScopeValidator(alert);
+            Assert.True(addressesRequiredWhenScopeValidator.IsValid);
+        }
+
+        [Fact]
+        public void AlertWithEmptyAddressesAndScopePrivateIsInvalid()
+        {
+            var alert = new Alert();
+            alert.Addresses = string.Empty;
+            alert.Scope = Scope.Private;
+
+            var addressesRequiredWhenScopeValidator = new AddressesRequiredWhenScopeValidator(alert);
+            Assert.False(addressesRequiredWhenScopeValidator.IsValid);
+        }
+    }
+}
---
+++ src/CAPNet/Validator/Alert/AddressesRequiredWhenScopeValidator.cs
@@ -0,0 +1,40 @@
+﻿using CAPNet.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CAPNet
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class AddressesRequiredWhenScopeValidator : Validator<Alert>
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="alert"></param>
+        public AddressesRequiredWhenScopeValidator(Alert alert) : base(alert) { }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public override bool IsValid
+        {
+            get 
+            {
+                return !(Entity.Scope == Scope.Private && string.IsNullOrEmpty(Entity.Addresses));
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public override IEnumerable<Error> Errors
+        {
+            get
+            {
+                yield return new AddressesRequiredWhenScopeError();
+            }
+        }
+    }
+}
