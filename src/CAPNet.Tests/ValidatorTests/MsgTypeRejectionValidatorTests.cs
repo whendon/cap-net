cb072bb88cad9713e872414d29cd2cbee7458fa8
vltadrian <adrian.vultur@gmail.com>
2014-07-04 17:04:53 +0300

    MsgType Rejection Validator

---
+++ src/CAPNet.Tests/ValidatorTests/MsgTypeRejectionValidatorTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xunit;
+using CAPNet.Tests;
+using CAPNet.Models;
+
+namespace CAPNet.Tests.ValidatorTests
+{
+    public class MsgTypeRejectionValidatorTests
+    {
+        [Fact]
+        public void MsgTypeWithErrorThatHasExplanationInNoteIsValid()
+        {
+            var alert = new Alert();
+            alert.MessageType = MessageType.Error;
+            alert.Note = "DescriptiveError";
+
+            var msgTypeRejectionValidator = new MsgTypeRejectionValidator(alert);
+            Assert.True(msgTypeRejectionValidator.IsValid);
+        }
+
+        [Fact]
+        public void MsgTypeWithErrorThatHasExplanationInNoteNullIsInvalid()
+        {
+            var alert = new Alert();
+            alert.MessageType = MessageType.Error;
+            alert.Note = null;
+
+            var msgTypeRejectionValidator = new MsgTypeRejectionValidator(alert);
+            Assert.False(msgTypeRejectionValidator.IsValid);
+        }
+
+        [Fact]
+        public void MsgTypeWithNoErrorThatHasExplanationInNoteIsValid()
+        {
+            var alert = new Alert();
+            alert.MessageType = MessageType.Cancel;
+            alert.Note = "Descriptive Explanation";
+
+            var msgTypeRejectionValidator = new MsgTypeRejectionValidator(alert);
+            Assert.True(msgTypeRejectionValidator.IsValid);
+        }
+
+        [Fact]
+        public void MsgTypeWithNoErrorThatHasNotExplanationInNoteIsValid()
+        {
+            var alert = new Alert();
+            alert.MessageType = MessageType.Cancel;
+            alert.Note = null;
+
+            var msgTypeRejectionValidator = new MsgTypeRejectionValidator(alert);
+            Assert.True(msgTypeRejectionValidator.IsValid);
+        }
+    }
+}
---
+++ src/CAPNet/Validator/Alert/MsgTypeRejectionValidator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using CAPNet.Models;
+
+namespace CAPNet
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class MsgTypeRejectionValidator : Validator<Alert>
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="alert"></param>
+        public MsgTypeRejectionValidator(Alert alert) : base(alert) { }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public override IEnumerable<Error> Errors
+        {
+            get
+            {
+                yield return new MsgTypeRejectionError();
+            }
+        }
+
+        /// <summary>
+        /// Message Type that is Error should have description in note
+        /// </summary>
+        public override bool IsValid
+        {
+            get 
+            {
+                return !Entity.MessageType.Equals(MessageType.Error) || (!string.IsNullOrEmpty(Entity.Note));
+            }
+        }
+    }
+}
