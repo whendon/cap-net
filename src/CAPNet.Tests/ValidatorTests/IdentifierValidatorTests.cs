1817c8f0c0a6f4cceb3b62e3c5027e8a9a23f95e
vltadrian <adrian.vultur@gmail.com>
2014-07-04 15:26:15 +0300

    Identifier Validator

---
+++ src/CAPNet.Tests/ValidatorTests/IdentifierValidatorTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xunit;
+using CAPNet.Tests;
+using CAPNet.Models;
+
+namespace CAPNet
+{
+    public class IdentifierValidatorTests
+    {
+        [Fact]
+        public void IdentifierWithValidNameIsValid()
+        {
+            var alert = new Alert();
+            alert.Identifier = "43b080713727";
+
+            var identifierValidator = new IdentifierValidator(alert);
+            Assert.True(identifierValidator.IsValid);
+            Assert.Equal(0, identifierValidator.Errors.Count());
+        }
+
+        [Fact]
+        public void IdentifierWithCommaIsInvalid()
+        {
+            var alert = new Alert();
+            alert.Identifier = "43b080713727,";
+
+            var identifierValidator = new IdentifierValidator(alert);
+            Assert.False(identifierValidator.IsValid);
+            Assert.Equal(1, identifierValidator.Errors.Count());
+        }
+    }
+}
---
+++ src/CAPNet/Validator/Errors/InvalidIdentifierError.cs
@@ -0,0 +1,9 @@
+﻿namespace CAPNet
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class InvalidIdentifierError:Error
+    {
+    }
+}
