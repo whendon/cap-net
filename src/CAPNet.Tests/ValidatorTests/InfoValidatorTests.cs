08f9fd903af1c4d7927c79cd0fa01392017b5aa6
vltadrian <adrian.vultur@gmail.com>
2014-07-07 14:44:25 +0300

    Info Elements and Sub-Elements Validator [ Without ResponseTypes ]

---
+++ src/CAPNet.Tests/ValidatorTests/InfoValidatorTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using CAPNet.Models;
+using Xunit;
+
+namespace CAPNet.Tests.ValidatorTests
+{
+    public class InfoValidatorTests
+    {
+        [Fact]
+        public void InfoWithRequiredFieldsIsValid()
+        {
+            Info info = new Info();
+            //Required Fields : Categories,Event,Urgency,Severity,Certainty
+            info.Categories.Add(Category.CBRNE);
+            info.Event = "Event";
+            info.Urgency = Urgency.Expected;
+            info.Severity = Severity.Extreme;
+            info.Certainty = Certainty.Likely;
+
+            Alert alert = new Alert();
+            alert.Info.Add(info);
+            var infoValidator = new InfoValidator(alert);
+            Assert.True(infoValidator.IsValid);
+        }
+
+        [Fact]
+        public void InfoWithRequiredFieldsExceptCertaintyIsInvalid()
+        {
+            Info info = new Info();
+            //Required Fields : Categories,Event,Urgency,Severity,Certainty
+            info.Categories.Add(Category.CBRNE);
+            info.Event = "Event";
+            info.Urgency = Urgency.Expected;
+            info.Severity = Severity.Extreme;
+            //info.Certainty is missing 
+
+            Alert alert = new Alert();
+            alert.Info.Add(info);
+            var infoValidator = new InfoValidator(alert);
+            Assert.False(infoValidator.IsValid);
+
+            var infoErrors = from error in infoValidator.Errors
+                             where error.GetType() == typeof(CertaintyRequiredError)
+                             select error;
+            Assert.NotEmpty(infoErrors);
+        }
+    }
+}
---
+++ src/CAPNet/Validator/Alert/Info/InfoValidator.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using CAPNet.Models;
+using System.Reflection;
+
+namespace CAPNet
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class InfoValidator : Validator<Alert>
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="alert"></param>
+        public InfoValidator(Alert alert) : base(alert) { }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public override IEnumerable<Error> Errors
+        {
+            get 
+            {
+                return from info in Entity.Info
+                       from error in GetErrors(info)
+                       select error;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="info"></param>
+        /// <returns></returns>
+        public IEnumerable<Error> GetErrors(Info info)
+        {
+            var infoValidators = from type in Assembly.GetExecutingAssembly().GetTypes()
+                                 where typeof(IValidator<Info>).IsAssignableFrom(type)
+                                 select (IValidator<Info>)Activator.CreateInstance(type, info);
+
+            return from validator in infoValidators
+                   from error in validator.Errors
+                   select error;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public override bool IsValid
+        {
+            get
+            {
+                return !Errors.Any();
+            }
+        }
+    }
+}
