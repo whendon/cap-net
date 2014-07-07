cc5de4ac62e52e0d40c6287abcee4695e44ffcf9
vltadrian <adrian.vultur@gmail.com>
2014-07-07 18:09:58 +0300

    Scope Required Validator

---
+++ src/CAPNet/Validator/Alert/ScopeRequiredValidator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using CAPNet.Models;
+
+namespace CAPNet
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ScopeRequiredValidator : Validator<Alert>
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="alert"></param>
+        public ScopeRequiredValidator(Alert alert) : base(alert) { }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public override IEnumerable<Error> Errors
+        {
+            get
+            {
+                if (!IsValid)
+                    yield return new ScopeRequiredError();
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public override bool IsValid
+        {
+            get
+            {
+                return Enum.IsDefined(typeof(Scope), this.Entity.Scope);
+            }
+        }
+    }
+}
