a4a745f188e4a0ba9af9b3b47db750b6ee5c1777
vltadrian <adrian.vultur@gmail.com>
2014-07-08 11:13:11 +0300

    Incidents Validator

---
+++ src/CAPNet.Tests/ValidatorTests/IncidentsValidatorTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xunit;
+using CAPNet.Tests;
+using CAPNet.Models;
+
+namespace CAPNet
+{
+    public class IncidentsValidatorTests
+    {
+        [Fact]
+        public void AlertWithIncidentNullIsValid()
+        {
+            var alert = new Alert();
+            alert.Incidents = null;
+
+            var incidentsValidator = new IncidentsValidator(alert);
+            Assert.True(incidentsValidator.IsValid);
+        }
+
+        [Fact]
+        public void AlertWithIncidentAndNoWhitespaceIsValid()
+        {
+            var alert = new Alert();
+            alert.Incidents = "incident1";
+
+            var incidentsValidator = new IncidentsValidator(alert);
+            Assert.True(incidentsValidator.IsValid);
+        }
+
+        [Fact]
+        public void AlertWithMultipleIncidentsAndSurroundedByDoubleQuotesIsValid()
+        {
+            var alert = new Alert();
+            alert.Incidents = "\"incident1\" \"incident2\" \"incident3\"";
+
+            var incidentsValidator = new IncidentsValidator(alert);
+            Assert.True(incidentsValidator.IsValid);
+        }
+
+        [Fact]
+        public void AlertWithMultipleIncidentsAndNotSurroundedByDoubleQuotesIsInvalid()
+        {
+            var alert = new Alert();
+            alert.Incidents = "incident1 inciden2 incident3";
+
+            var incidentsValidator = new IncidentsValidator(alert);
+            Assert.False(incidentsValidator.IsValid);
+            Assert.Equal(typeof(IncidentsError), incidentsValidator.Errors.ElementAt(0).GetType());
+        }
+    }
+}
---
+++ src/CAPNet/Validator/Alert/IncidentsValidator.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using CAPNet.Models;
+
+namespace CAPNet
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class IncidentsValidator : Validator<Alert>
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="alert"></param>
+        public IncidentsValidator(Alert alert) : base(alert) { }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public override IEnumerable<Error> Errors
+        {
+            get
+            {
+                if (!IsValid)
+                    yield return new IncidentsError();
+            }
+        }
+        
+        /// <summary>
+        /// An incident validator is not required . But when it is used , it should respect the rules : 
+        ///      1. If it doesn't contain any whitespace , it is valid anyway
+        ///      2. If it contains any whitespace , each incident name should be surrounded by double-quotes.
+        /// </summary>
+        public override bool IsValid
+        {
+            get
+            {
+                if (!string.IsNullOrEmpty(Entity.Incidents))
+                {
+                    if (Entity.Incidents.Contains(" "))
+                    {
+                        var invalidIncident = from incident in Entity.Incidents.Split(' ')
+                                              where (incident[0] != '\"' || incident[incident.Length - 1] != '\"')
+                                              select incident;
+                        return !invalidIncident.Any();
+                    }
+                    else return true; // If it doesn't contain any whitespace , it is valid anyway
+                }
+                else return true; // If it doesn't exist , it is valid anyway
+            }
+        }
+    }
+}
