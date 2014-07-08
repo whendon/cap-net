57be0563a6077f31ad5d4bb884d0bcfd41201591
vltadrian <adrian.vultur@gmail.com>
2014-07-08 09:46:49 +0300

    Restriction Optional Use Validator

---
+++ src/CAPNet/Validator/Alert/RestrictionValidator.cs
@@ -0,0 +1,43 @@
+﻿using CAPNet.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CAPNet
+{
+    /// <summary>
+    /// Restriction is used only when Alert.Scope is Scope.Restricted
+    /// </summary>
+    public class RestrictionValidator : Validator<Alert>
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="alert"></param>
+        public RestrictionValidator(Alert alert) : base(alert) { }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public override IEnumerable<Error> Errors
+        {
+            get
+            {
+                if (!IsValid)
+                    yield return new RestrictionError();
+            }
+        }
+
+        /// <summary>
+        /// The restriction validator is valid in two cases :
+        ///     1. Alert.Scope is Restricted and Restriction is not null
+        ///     2. Alert.Scope is not Restricted and Restriction is null
+        /// </summary>
+        public override bool IsValid
+        {
+            get
+            {
+                return ((Entity.Scope == Scope.Restricted && Entity.Restriction != null) || (Entity.Scope != Scope.Restricted && string.IsNullOrEmpty(Entity.Restriction)));
+            }
+        }
+    }
+}
