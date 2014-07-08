ad8b629d3f2c4e34344a1166d15eeea8cad41b28
vltadrian <adrian.vultur@gmail.com>
2014-07-08 13:10:17 +0300

    New General Alert Validator Test Added

--- src/CAPNet.Tests/ValidatorTests/AlertValidatorTests.cs
+++ src/CAPNet.Tests/ValidatorTests/AlertValidatorTests.cs
@@ -7,7 +7,7 @@ namespace CAPNet.Tests.ValidatorTests
     public class AlertValidatorTests
     {
         [Fact]
-        public void GeneralValidAlert()
+        public void GeneralAlertWithAllRequiredFieldsIsValid()
         {
             var alert = new Alert();
             alert.Identifier = "KSTO1055887203";
@@ -35,6 +35,42 @@ namespace CAPNet.Tests.ValidatorTests
 
             var alertValidator = new AlertValidator(alert);
             Assert.True(alertValidator.IsValid);
+        }
+
+        [Fact]
+        public void GeneralAlertWithWrongCategoryInInfoIsInvalid()
+        {
+            var alert = new Alert();
+            alert.Identifier = "KSTO1055887203";
+            alert.Sender = "Sender";
+            alert.Status = Status.Draft;
+            alert.MessageType = MessageType.Error;
+            alert.Note = "DescriptiveError";
+            alert.Scope = Scope.Restricted;
+            alert.Restriction = "Draft";
+            alert.Sent = new System.DateTimeOffset();
+
+            var info = new Info();
+            //invalid category set : 
+            var category = (Category)123;
+            //Category required
+            info.Categories.Add(category);
+            //Certainty required
+            info.Certainty = Certainty.Observed;
+            //EventRequired
+            info.Event = "ImportantEvent";
+            //SeverityRequired
+            info.Severity = Severity.Minor;
+            //UrgencyRequired
+            info.Urgency = Urgency.Future;
+            alert.Info.Add(info);
+
+            var alertValidator = new AlertValidator(alert);
+            Assert.False(alertValidator.IsValid);
+            var categoryErrors = from error in alertValidator.Errors
+                                where typeof(InvalidCategoryError) == error.GetType()
+                                select error;
+            Assert.NotEmpty(categoryErrors);
         }
     }
 }
