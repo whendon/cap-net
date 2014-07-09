8e0f93f73401f6db93b02f5dd066fb47cd986f28
vltadrian <adrian.vultur@gmail.com>
2014-07-09 14:10:56 +0300

    Event Code Validator

---
+++ src/CAPNet/Validator/Alert/Info/EventCodeValidator.cs
@@ -0,0 +1,43 @@
+﻿using CAPNet.Models;
+using System.Collections.Generic;
+
+namespace CAPNet
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class EventCodeValidator : Validator<EventCode>
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="eventCode"></param>
+        public EventCodeValidator(EventCode eventCode) : base(eventCode) { }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public override IEnumerable<Error> Errors
+        {
+            get
+            {
+                if (!IsValid)
+                    yield return new EventCodeError();
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public override bool IsValid
+        {
+            get
+            {
+                var valueIsNotNull = Entity.Value != null;
+                var valueNameIsNotNull = Entity.ValueName != null;
+
+                return valueIsNotNull && valueNameIsNotNull;
+            }
+        }
+    }
+}
