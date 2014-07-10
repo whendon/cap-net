952659e5e19c283180604d746ca0cd09a6c22335
StanculescuDaniel <danstan01@gmail.com>
2014-07-10 12:41:48 +0300

    Separate method for each different state

---
+++ src/CAPNet/XmlSpaceDelimitedElementsParser.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace CAPNet
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class XmlSpaceDelimitedElementsParser
+    {
+
+        private const string decisionState = "spaceState";
+        private const string addCharInSpaceContainingAddress = "addCharInSpaceContainingAddress";
+        private const string addCharInAddressWithNoSpace = "addCharInAddressWithNoSpace";
+        private static string state;
+        private static string partialElement;
+        private static int i;
+        private static char[] representationChars;
+        private static List<string> addresses;
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="representation"></param>
+        /// <returns></returns>
+        public static IEnumerable<string> GetElements(this string representation)
+        {
+            addresses = new List<string>();
+            representationChars = representation.ToCharArray();
+            state = decisionState;
+            partialElement = "";
+
+            for (i = 0; i < representationChars.Count(); i++)
+            {
+                char currentChar = representationChars[i];
+                switch (state)
+                {
+                    case decisionState:
+                        DecisionState(currentChar);
+                        break;
+
+                    case addCharInAddressWithNoSpace:
+                        AddCharInAddressWithNoSpaceState(currentChar);
+                        break;
+
+                    case addCharInSpaceContainingAddress:
+
+                        AddCharInSpaceContainingAddressState(currentChar);
+                        break;
+                }
+            }
+
+            return addresses;
+        }
+
+        private static void AddCharInSpaceContainingAddressState(char currentChar)
+        {
+            if (currentChar.IsElementCaracter() || currentChar.IsSpace())
+            {
+                state = addCharInSpaceContainingAddress;
+                partialElement += currentChar;
+                if (i == representationChars.Count() - 1)
+                {
+                    addresses.Add(partialElement);
+                }
+            }
+            else if (currentChar.IsQuote())
+            {
+                addresses.Add(partialElement);
+                partialElement = "";
+                state = decisionState;
+            }
+        }
+
+        private static void AddCharInAddressWithNoSpaceState(char currentChar)
+        {
+            if (currentChar.IsElementCaracter())
+            {
+                state = addCharInAddressWithNoSpace;
+                partialElement += currentChar;
+                if (i == representationChars.Count() - 1)
+                {
+                    addresses.Add(partialElement);
+                }
+            }
+            else if (currentChar.IsSpace())
+            {
+                addresses.Add(partialElement);
+                partialElement = "";
+                state = decisionState;
+            }
+            else if (currentChar.IsQuote())
+            {
+                state = addCharInSpaceContainingAddress;
+            }
+        }
+
+        private static void DecisionState(char currentChar)
+        {
+            if (currentChar.IsSpace())
+            {
+                state = addCharInAddressWithNoSpace;
+            }
+            else if (currentChar.IsQuote())
+            {
+                state = addCharInSpaceContainingAddress;
+            }
+            else if (currentChar.IsElementCaracter())
+            {
+                state = addCharInAddressWithNoSpace;
+                partialElement += currentChar;
+            }
+        }
+
+        private static bool IsElementCaracter(this char tested)
+        {
+            return tested.CompareTo('"') != 0 && tested.CompareTo(' ') != 0;
+        }
+
+        private static bool IsSpace(this char tested)
+        {
+            return tested.CompareTo(' ') == 0;
+        }
+
+        private static bool IsQuote(this char tested)
+        {
+            return tested.CompareTo('"') == 0;
+        }
+
+    }
+
+}
