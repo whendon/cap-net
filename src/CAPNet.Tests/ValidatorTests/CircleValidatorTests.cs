67d1b70b91255d968482e434d5ab2fd069235855
vltadrian <adrian.vultur@gmail.com>
2014-07-10 11:09:25 +0300

    Circle Validator

---
+++ src/CAPNet.Tests/ValidatorTests/CircleValidatorTests.cs
@@ -0,0 +1,38 @@
+﻿using CAPNet.Models;
+using System.Linq;
+using Xunit;
+
+namespace CAPNet
+{
+    public class CircleValidatorTests
+    {
+        [Fact]
+        public void CircleWithInvalidCoordinatesIsInvalid()
+        {
+            var area = new Area();
+            var coordinate = new Coordinate(400, 400);
+            var circle = new Circle(coordinate, 14);
+            area.Circles.Add(circle);
+
+            var circleValidator = new CircleValidator(area);
+            Assert.False(circleValidator.IsValid);
+
+            var errors = from error in circleValidator.Errors
+                         where error.GetType() == typeof(WGS84Error)
+                         select error;
+            Assert.NotEmpty(errors);
+        }
+
+        [Fact]
+        public void CircleWithValidCoordinatesIsValid()
+        {
+            var area = new Area();
+            var coordinate = new Coordinate(50, 50);
+            var circle = new Circle(coordinate, 55);
+            area.Circles.Add(circle);
+
+            var circleValidator = new CircleValidator(area);
+            Assert.True(circleValidator.IsValid);
+        }
+    }
+}
---
+++ src/CAPNet/Validator/Alert/Area/CircleValidator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CAPNet.Models;
+
+namespace CAPNet
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class CircleValidator : Validator<Area>
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="area"></param>
+        public CircleValidator(Area area) : base(area) { }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public override IEnumerable<Error> Errors
+        {
+            get
+            {
+                var wgs84Validators = from circle in Entity.Circles
+                                      select new WGS84Validator(circle.Center);
+
+                return from wgs84Validator in wgs84Validators
+                       from error in wgs84Validator.Errors
+                       select error;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public override bool IsValid
+        {
+            get
+            {
+                return !Errors.Any();
+            }
+        }
+    }
+}
