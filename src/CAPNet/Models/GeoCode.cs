aad257371d49b51f67fdd5227032443b35410706
StanculescuDaniel <danstan01@gmail.com>
2014-07-11 12:09:01 +0300

    GeoCode commented

--- src/CAPNet/Models/GeoCode.cs
+++ src/CAPNet/Models/GeoCode.cs
@@ -6,10 +6,10 @@
     public class GeoCode : NamedValue
     {
         /// <summary>
-        /// 
+        /// The geographic code delineating the affected area of the alert message 
         /// </summary>
-        /// <param name="valueName"></param>
-        /// <param name="value"></param>
+        /// <param name="valueName">a user-assigned string designating the domain of the code</param>
+        /// <param name="value">a string (which may represent a number) denoting the value itself (e.g., valueName ="SAME" and value="006113").</param>
         public GeoCode(string valueName, string value)
             : base(valueName, value)
         {
