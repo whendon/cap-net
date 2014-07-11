63fcc2389a71e54aeab2531b5c83417ba22901e4
StanculescuDaniel <danstan01@gmail.com>
2014-07-11 11:49:03 +0300

    Parameter commented

--- src/CAPNet/Models/Parameter.cs
+++ src/CAPNet/Models/Parameter.cs
@@ -8,8 +8,8 @@
         /// <summary>
         /// 
         /// </summary>
-        /// <param name="valueName"></param>
-        /// <param name="value"></param>
+        /// <param name="valueName">user-assigned string designating the domain of the code</param>
+        /// <param name="value">is a string (which may represent a number) denoting the value itself (e.g., valueName ="SAME" and value="CIV").</param>
         public Parameter(string valueName, string value)
             : base(valueName, value)
         {
