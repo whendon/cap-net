d8c5afee14ccc67f5b3f1f0a5ce6f96ca573ea8a
Victor Grigoriu <vgrigoriu@gmail.com>
2015-03-03 19:54:30 +0200

    Add nuspec, change assembly metadata

--- src/CAPNet/Properties/AssemblyInfo.cs
+++ src/CAPNet/Properties/AssemblyInfo.cs
@@ -3,9 +3,9 @@
 [assembly: AssemblyTitle("cap-net")]
 [assembly: AssemblyDescription("A Common Alerting Protocol serialization library for the .NET framework.")]
 [assembly: AssemblyProduct("cap-net")]
-[assembly: AssemblyCopyright("Copyright © 2013 Matt Chandler and contributors")]
+[assembly: AssemblyCopyright("Copyright © 2015 Matt Chandler and contributors")]
 
-
+[assembly: AssemblyCompany("Teamnet")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: AssemblyInformationalVersion("1.0.0-alpha")]
