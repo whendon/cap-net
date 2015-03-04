6e00c1bf16ec62939a2398da75ff266f6f65bd48
Victor Grigoriu <vgrigoriu@gmail.com>
2015-03-04 16:28:06 +0200

    Add printfs in test for debugging

--- src/CAPNet.Tests/XmlCreatorTests.cs
+++ src/CAPNet.Tests/XmlCreatorTests.cs
@@ -99,6 +99,27 @@ namespace CAPNet.Tests
                 alertAsString = new StreamReader(memoryStream, Encoding.UTF8).ReadToEnd();
             }
 
+            Console.WriteLine("Expected char at 37 is {0}",
+                Convert.ToInt32(
+                    Xml.MultipleAlertXml.ToCharArray()[37]));
+            Console.WriteLine("Actual char at 37 is {0}",
+                Convert.ToInt32(
+                    alertAsString.ToCharArray()[37]));
+
+            Console.WriteLine("Expected char at 38 is {0}",
+                Convert.ToInt32(
+                    Xml.MultipleAlertXml.ToCharArray()[38]));
+            Console.WriteLine("Actual char at 38 is {0}",
+                Convert.ToInt32(
+                    alertAsString.ToCharArray()[38]));
+
+            Console.WriteLine("Expected char at 39 is {0}",
+                Convert.ToInt32(
+                    Xml.MultipleAlertXml.ToCharArray()[39]));
+            Console.WriteLine("Actual char at 39 is {0}",
+                Convert.ToInt32(
+                    alertAsString.ToCharArray()[39]));
+
             Assert.Equal(Xml.MultipleAlertXml, alertAsString);
 
         }
