b2c8dc3813bb19f58dfffc4a31c84673d29a7900
Victor Grigoriu <vgrigoriu@gmail.com>
2016-02-02 13:51:24 +0200

    Cosmetics

--- src/CAPNet.Tests/XmlParserTests.cs
+++ src/CAPNet.Tests/XmlParserTests.cs
@@ -1,19 +1,17 @@
 ﻿using System;
-using System.Globalization;
 using System.Linq;
 using System.Collections.Generic;
 
 using CAPNet.Models;
 
 using Xunit;
-using System.Xml.Linq;
 
 namespace CAPNet.Tests
 {
     public class XmlParserTests
     {
         [Fact]
-        public void CanReadMultipleAlertsFromXML()
+        public void CanReadMultipleAlertsFromXml()
         {
             var alertList = XmlParser.Parse(Xml.MultipleThunderstorm12Xml);
             Assert.Equal(2, alertList.Count());
@@ -113,7 +111,7 @@ namespace CAPNet.Tests
             Assert.NotNull(alert);
 
             var circles = alert.Info.ElementAt(0).Areas.ElementAt(0).Circles;
-            Assert.Equal(1, circles.Count());
+            Assert.Equal(1, circles.Count);
 
             //<circle>32.9525,-115.5527 0</circle>
             Assert.Equal("32.9525,-115.5527 0", circles.First().ToString());
@@ -140,7 +138,7 @@ namespace CAPNet.Tests
             Assert.NotNull(alert);
 
             var circles = alert.Info.ElementAt(0).Areas.ElementAt(0).Circles;
-            Assert.Equal(2, circles.Count());
+            Assert.Equal(2, circles.Count);
 
             //<circle>32.9525,-115.5527 0</circle>  
             Assert.Equal("32.9525,-115.5527 0", circles.First().ToString());
@@ -165,7 +163,7 @@ namespace CAPNet.Tests
             Assert.NotNull(alert);
 
             var polygons = alert.Info.ElementAt(0).Areas.ElementAt(0).Polygons;
-            Assert.Equal(1, polygons.Count());
+            Assert.Equal(1, polygons.Count);
 
             //<polygon>38.47,-120.14 38.34,-119.95 38.52,-119.74 38.62,-119.89 38.47,-120.14</polygon>
             Assert.Equal("38.47,-120.14 38.34,-119.95 38.52,-119.74 38.62,-119.89 38.47,-120.14", polygons.First().ToString());
@@ -178,7 +176,7 @@ namespace CAPNet.Tests
             Assert.NotNull(alert);
 
             ICollection<GeoCode> geoCodes = alert.Info.ElementAt(0).Areas.ElementAt(0).GeoCodes;
-            Assert.Equal(3, geoCodes.Count());
+            Assert.Equal(3, geoCodes.Count);
 
 
             //<geocode>
@@ -211,7 +209,7 @@ namespace CAPNet.Tests
             Assert.NotNull(alert);
 
             var polygons = alert.Info.ElementAt(0).Areas.ElementAt(0).Polygons;
-            Assert.Equal(2, polygons.Count());
+            Assert.Equal(2, polygons.Count);
 
             //<polygon>38.47,-120.14 38.34,-119.95 38.52,-119.74 38.62,-119.89 38.47,-120.14</polygon>
             var firstPolygon = polygons.First();
--- src/CAPNet/Models/Coordinate.cs
+++ src/CAPNet/Models/Coordinate.cs
@@ -14,10 +14,14 @@ namespace CAPNet.Models
         /// <param name="stringRepresentation"></param>
         public Coordinate(string stringRepresentation)
         {
-            if (stringRepresentation == null) { throw new ArgumentNullException(nameof(stringRepresentation)); }
+            if (stringRepresentation == null)
+            {
+                throw new ArgumentNullException(nameof(stringRepresentation));
+            }
+
             var splitCoordinate = stringRepresentation.Split(',');
-            this.Latitude = decimal.Parse(splitCoordinate[0], CultureInfo.InvariantCulture);
-            this.Longitude = decimal.Parse(splitCoordinate[1], CultureInfo.InvariantCulture);
+            Latitude = decimal.Parse(splitCoordinate[0], CultureInfo.InvariantCulture);
+            Longitude = decimal.Parse(splitCoordinate[1], CultureInfo.InvariantCulture);
         }
 
         /// <summary>
@@ -32,22 +36,14 @@ namespace CAPNet.Models
         }
 
         /// <summary>
-        /// 
+        /// The latitude part of this coordinate, in WGS 84.
         /// </summary>
-        public decimal Latitude
-        {
-            get;
-            private set;
-        }
+        public decimal Latitude { get; }
 
         /// <summary>
-        /// 
+        /// The longitude part of this coordinate, in WGS 84.
         /// </summary>
-        public decimal Longitude
-        {
-            get;
-            private set;
-        }
+        public decimal Longitude { get; }
 
         /// <summary>
         /// 
@@ -67,13 +63,13 @@ namespace CAPNet.Models
         /// </summary>
         /// <param name="obj"></param>
         /// <returns></returns>
-        public override bool Equals(Object obj)
+        public override bool Equals(object obj)
         {
             if (obj == null || GetType() != obj.GetType())
                 return false;
 
-            Coordinate p = (Coordinate)obj;
-            return (this.Latitude == p.Latitude && this.Longitude == p.Longitude);
+            var other = (Coordinate)obj;
+            return Latitude == other.Latitude && Longitude == other.Longitude;
         }
 
         /// <summary>
